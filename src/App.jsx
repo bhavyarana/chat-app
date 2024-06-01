0691c0e8bafcf2e130f85812449063cf9e85272d
Bhavya Rana <bhavya.rana10@gmail.com>
2024-06-01 12:17:13 +0530

    Added work on progress page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import React from 'react';
-import Home from './assets/pages/Home';
+import React from "react";
+import Home from "./assets/pages/Home";
 import "../src/style.scss";
+import WorkOnProgress from "./assets/components/WorkOnProgress";
 
 const App = () => {
   return (
     <>
-      <Home/>
+      {/* <Home/> */}
+      <WorkOnProgress />
     </>
-  )
-}
+  );
+};
 
 export default App;
---
+++ src/assets/components/WorkOnProgress.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const WorkOnProgress = () => {
+  return (
+    <div className="work-on-progress">
+      <div>Work On Progress</div>
+      <div>Please try after some time... </div>
+    </div>
+  );
+};
+
+export default WorkOnProgress;
